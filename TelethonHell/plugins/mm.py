542db9b0d8766dfae6affb33922870d386f9cb10
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-18 15:21:16 +0530

    Update mm.py

--- TelethonHell/plugins/mm.py
+++ TelethonHell/plugins/mm.py
@@ -5,3 +5,7 @@ async def _(event):
   a = await bot.get_messages(event.chat_id, 0, from_user="me")
   b = await bot.get_messages(event.chat_id)
   await event.edit(f" Your Total messages = {a.total}\n\n And Total group messages : {b.total}")
+
+CmdHelp("mm").add_command(
+    "mm", "Count your send messages and Total group messages"
+).add()
