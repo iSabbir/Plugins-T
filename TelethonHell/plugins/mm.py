6984f052d0f2c92e02f32f4c890b8c802e9aace7
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-18 15:19:33 +0530

    Add files via upload

---
+++ TelethonHell/plugins/mm.py
@@ -0,0 +1,7 @@
+from . import *
+
+@hell_cmd(pattern="mm")
+async def _(event):
+  a = await bot.get_messages(event.chat_id, 0, from_user="me")
+  b = await bot.get_messages(event.chat_id)
+  await event.edit(f" Your Total messages = {a.total}\n\n And Total group messages : {b.total}")
